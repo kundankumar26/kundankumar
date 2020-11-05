54ce3a43c00f3958b94c384e8cc93effb31d4271
Kundan Kumar <rockingkundan26@gmail.com>
2020-11-06 05:08:36 +0530

    fourth commit

--- public/javascript/header.js
+++ public/javascript/header.js
@@ -3,61 +3,60 @@ $(document).ready(function(){
   // For Navigation Bar collapse
   var scrollTop = 0;
   $(window).scroll(function(){
-    scrollTop = $(window).scrollTop();
+      scrollTop = $(window).scrollTop();
       $('.counter').html(scrollTop);
-    
-    if (scrollTop >= 100) {
-      $('.navigation').addClass('scrolled-navbar');
-    } else if (scrollTop < 100) {
-      $('.navigation').removeClass('scrolled-navbar');
-    }    
+      
+      if (scrollTop >= 100) {
+          $('.navigation').addClass('scrolled-navbar');
+      } else if (scrollTop < 100) {
+          $('.navigation').removeClass('scrolled-navbar');
+      }    
   }); 
 
   //Intialising the web page with default activate class
   $(function(){
-    classAddActivate();
+      classAddActivate();
   })
 
   //Add strike-through effect to the current item
   $(".nav-list li a").hover(
-    function () {
-      classRemoveActivate();
-      $(this).addClass('activate');
-    },
-    function () {
-      $(this).removeClass("activate");
-      classAddActivate();
-    }
+      function () {
+          classRemoveActivate();
+          $(this).addClass('activate');
+      },
+      function () {
+          $(this).removeClass("activate");
+          classAddActivate();
+      }
   );
 
   // Add the activate Class
   function classAddActivate(){
-    var current = location.pathname;
-    $('.nav-list li a').each(function(){
-        var $this = $(this);
-        // if the current path is like this link, make it active
-        if($this.attr('href').indexOf(current) != -1){
-            $this.addClass('activate');
-        }
-    })
+      var current = location.pathname;
+      $('.nav-list li a').each(function(){
+          var $this = $(this);
+          // if the current path is like this link, make it active
+          if($this.attr('href').indexOf(current) != -1){
+              $this.addClass('activate');
+          }
+      })
   }
 
   //Remove the activate Class
   function classRemoveActivate(){
-    var current = location.pathname;
-    $('.nav-list li a').each(function(){
-      var $this = $(this);
-      // if the current path is like this link, make it active
-      if($this.attr('href').indexOf(current) != -1){
-        $this.removeClass('activate');
-      }
-    })
+      var current = location.pathname;
+      $('.nav-list li a').each(function(){
+          var $this = $(this);
+          // if the current path is like this link, make it active
+          if($this.attr('href').indexOf(current) != -1){
+              $this.removeClass('activate');
+          }
+      })
   }
 });
 
-
 $('#nav-toggle').click(function() {
-  $('nav ul').slideToggle();
+    $('nav ul').slideToggle();
 });
     
     
